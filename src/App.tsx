f7b12c9224c3536228bae198aba094b3dd1d616d
Kelly Cuevas <41708498+KellyCuevas@users.noreply.github.com>
2023-07-29 11:11:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import "./styles.css";
+
+type Base = {
+  id: string;
+  title: string;
+};
+
+type GenericSelectProps<TValue> = {
+  values: TValue[];
+  onChange: (value: TValue) => void;
+};
+
+export const GenericSelect = <TValue extends Base>({
+  values,
+  onChange
+}: GenericSelectProps<TValue>) => {
+  const onSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const val = values.find((value) => value.id === e.target.value);
+
+    if (val) onChange(val);
+  };
+
+  return (
+    <select onChange={onSelectChange}>
+      {values.map((value) => (
+        <option key={value.id} value={value.id}>
+          {value.title}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export type Book = {
+  id: string;
+  title: string;
+  author: string;
+};
+
+export type Movie = {
+  id: string;
+  title: string;
+  releaseDate: string;
+};
+
+const books: Book[] = [
+  {
+    id: "1",
+    title: "Good omens",
+    author: "Terry Pratchett & Neil Gaiman"
+  },
+  {
+    id: "2",
+    title: "The Truth",
+    author: "Terry Pratchett"
+  }
+];
+
+const movies: Movie[] = [
+  {
+    id: "1",
+    title: "Captain Marvel",
+    releaseDate: "2019"
+  },
+  {
+    id: "2",
+    title: "Good Omens",
+    releaseDate: "2019"
+  }
+];
+
+export const AmazonCloneWithState = () => {
+  const [book, setBook] = useState<Book>(books[0]);
+  const [movie, setMovie] = useState<Movie>(movies[0]);
+
+  return (
+    <>
+      Selected book: {book.title} by {book.author}
+      <br />
+      <br />
+      Selected movie: {movie.title}, released in {movie.releaseDate}
+      <br />
+      <br />
+      Books:
+      <br />
+      <GenericSelect<Book>
+        onChange={(value) => setBook(value)}
+        values={books}
+      />
+      <br />
+      <br />
+      Movies:
+      <br />
+      <GenericSelect<Movie>
+        onChange={(value) => setMovie(value)}
+        values={movies}
+      />
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <AmazonCloneWithState />
+    </div>
+  );
+}
